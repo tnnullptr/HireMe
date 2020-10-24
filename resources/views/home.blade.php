b1ca7a8cc9387b96393d31d955071c5f08fdd2d7
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2020-10-25 01:36:39 +0800

    Update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,7 +19,7 @@
                             <h5 class="card-title">{{$job['name']}}</h5>
                             <h6 class="card-subtitle mb-2 text-muted"><i class="fa fa-money"></i>: {{$job['salary']}}</h6>
                             <p class="card-text"><i class="fa fa-location-arrow"></i> {{$job['location']}} </p>
-                            <p class="card-text"><i class="fa fa-bookmark"></i> {{$job['type'] | }} </p>
+                            <p class="card-text"><i class="fa fa-bookmark"></i> {{$job['type'] }} </p>
                             <p class="card-text">{{$job['context']}} </p>
                         </div>
                     </div>
