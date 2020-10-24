d53d7a58ab4ec7cf52040f1570cc23f325b4cf6d
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2020-10-24 09:50:12 +0800

    [Add] Create User Model.

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Str;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('adminadmin'),
+            'api_token' => Str::random(60),
+        ]);
+    }
+}
