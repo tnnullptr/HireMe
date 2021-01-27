2b1b0aaf8bbf41f6cc1683da2fec697acd7381e1
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2021-01-27 23:25:45 +0800

    [Fix] Fix usort problem

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -66,7 +66,9 @@ class HomeController extends Controller
                 if(in_array($uskill,$a['data']) && in_array($uskill,$b['data'])){
                     continue;
                 }else{
-                    return in_array($uskill,$a['data']) > in_array($uskill,$b['data']);
+                    if(in_array($uskill,$a['data']) > in_array($uskill,$b['data'])){
+                        return 1;
+                    }
                 }
             }
             return 1;
